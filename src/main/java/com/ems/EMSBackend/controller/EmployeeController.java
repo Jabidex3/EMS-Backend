94f1d3af2c548632d1ae435a64c030cff7433490
Jabid <jabidmethun@gmail.com>
2021-01-26 17:12:46 -0500

    added delete employee route

--- src/main/java/com/ems/EMSBackend/controller/EmployeeController.java
+++ src/main/java/com/ems/EMSBackend/controller/EmployeeController.java
@@ -3,8 +3,11 @@ package com.ems.EMSBackend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,21 +25,32 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeService es;
 	
-	@GetMapping("test")
-	public String test(){
-		return "Hello World";
-	}
-	
+	//get all employees
 	@GetMapping("employees")
 	public List<Employee> getEmployees(){
 		return this.es.findAll();
 	}
 	
-	@PostMapping("employee")
+	//add a new employee
+	@PostMapping("add")
 	public Employee addEmployee(@RequestBody Employee e){
 //		Employee newEmp = new Employee();
 //		return newEmp;
 		es.addEmployee(e);
 		return e;
+	}
+	
+	//delete an existing employee
+	@DeleteMapping("delete/{id}")
+	public boolean deleteEmployee(@PathVariable int id) { 
+		List<Employee> allEmp = es.findAll();
+		for(int i =0;i<allEmp.size();i++) {
+			if(allEmp.get(i).getId()==id) {
+				allEmp.remove(i);
+				return true;
+			}
+		}
+		
+		return false; //not found
 	}
 }
